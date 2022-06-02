ca579ca508b883934b24c222c2cf261ccae854bf
Lautaro Roldan <lautaronicolasroldan@gmail.com>
2022-06-02 16:46:50 -0300

    Create PupilServiceImplTest.java
    elijo la clase mas especifica para crear los test

---
+++ src/test/java/com/example/administrator/administrator/service/impl/PupilServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.example.administrator.administrator.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PupilServiceImplTest {
+
+    @Test
+    void addPupil() {
+    }
+
+    @Test
+    void deletePupil() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void getAllPupils() {
+    }
+}
